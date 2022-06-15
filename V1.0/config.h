18c55682b0fbe341d9b64b7a46a2f0f5e59b20dc
Sperotto <sperotto.lucas@gmail.com>
2022-06-14 23:49:32 -0300

    Add files via upload

---
+++ V1.0/config.h
@@ -0,0 +1,9 @@
+#define IO_USERNAME "USERNAME"
+#define IO_KEY "KEY"
+
+#define WIFI_SSID "SSID"
+#define WIFI_PASS "PASS"
+
+#include "AdafruitIO_WiFi.h"
+
+AdafruitIO_WiFi io(IO_USERNAME, IO_KEY, WIFI_SSID, WIFI_PASS);
